fa464a0e909a5faca9f2825da6d8a70ab3af8a6d
a179346 <r122164152@gmail.com>
2020-04-18 19:25:01 +0800

    add demo2

---
+++ demo/demo2.js
@@ -0,0 +1,31 @@
+const Validatar = new (require('validatar'))();
+
+Validatar.register('required', new Error('Wrong input format'), (v) => v !== undefined);
+Validatar.register('isString', '%{key} is not a string. value:%{value}', (v) => (typeof (v) === 'string'));
+Validatar.register('isEmail', '%{key} is not email. value:%{value}', (v) => (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(String(v).toLowerCase())));
+
+const correctData = {
+  str1: 'I am a string!',
+  obj1: {
+    email: 'random@email.com',
+  },
+};
+
+const rule = {
+  str1: [{ constraint: 'isString', message: 'str1 must be string' }],
+  obj1: {
+    email: ['required', 'isString', 'isEmail'],
+  },
+};
+
+const correctResult = Validatar.validate(correctData, rule);
+console.log(correctResult);
+// expected output: undefined
+
+const wrongData = {
+  str1: 'I am a string!',
+};
+
+const wrongResult = Validatar.validate(wrongData, rule);
+console.log(wrongResult);
+// expected output: Error: Wrong input format
