3373706b471cec69b02c20708d9b8ecc584366df
a179346 <r122164152@gmail.com>
2020-04-19 16:17:07 +0800

    change return message

--- Constraint.js
+++ Constraint.js
@@ -15,17 +15,14 @@ Constraint.prototype.check = function (value, params, message, key, position) {
       value,
       params,
     };
-    result.message = (
-      message
-      || (
-        typeof (this.message) === 'string'
-          ? this.message.replace(/%\{key\}/g, key)
-            .replace(/%\{value\}/g, value)
-            .replace(/%\{position\}/g, position)
-            .replace(/%\{constraintId\}/g, constraintId)
-          : this.message
-      )
-    );
+    result.message = message || this.message;
+    if (typeof (result.message) === 'string') {
+      result.message = result.message
+        .replace(/%\{key\}/g, key)
+        .replace(/%\{value\}/g, value)
+        .replace(/%\{position\}/g, position)
+        .replace(/%\{constraintId\}/g, constraintId);
+    }
     return result;
   }
   return undefined;
